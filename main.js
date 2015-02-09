6d20e468af9d4b73396ce80b835f0eeaa8b8d2f0
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2015-02-09 15:01:03 -0500

    First swing

--- main.js
+++ main.js
@@ -10,18 +10,32 @@ define(function (require, exports, module) {
         PreferencesManager   = brackets.getModule("preferences/PreferencesManager"),
         ProjectManager       = brackets.getModule("project/ProjectManager"),
         browser              = require("lib/iframe-browser");
+        LiveDevelopment      = brackets.getModule("LiveDevelopment/LiveDevMultiBrowser"),
+        // PostMessageTransport = require("LiveDevelopment/MultiBrowserImpl/transports/PostMessageTransport"),
         NoHostServer         = require("nohost/src/NoHostServer").NoHostServer;
-    
+
     function _createServer() {
         var config = {
             pathResolver    : ProjectManager.makeProjectRelativeIfPossible,
             root            : ProjectManager.getProjectRoot().fullPath
         };
-        
+
         return new NoHostServer(config);
     }
 
-    AppInit.appReady(function () {
+    function _configureIframeForUse() {
+        // Set transport
+            // PostMessageTransport.setIframe(browser.getIframe());
+            // LiveDevelopment.setTransport(PostMessageTransport);
+    }
+    LiveDevelopment.one("statusChange", _configureIframeForUse);
+
+    function _beginLivePreview() {
+        // LiveDevelopment.open()
+    }
+    ProjectManager.one("projectOpen", _beginLivePreview);
+
+    AppInit.extensionsLoaded(function () {
         // Flip livedev.multibrowser to true
         var prefs = PreferencesManager.getExtensionPrefs("livedev");
         prefs.set("multibrowser", true);
@@ -32,5 +46,23 @@ define(function (require, exports, module) {
         // Turn preview iFrame On
         browser.browse();
     });
-    
+
+        // XXXnote: This block of code will need to live in a Launcher of some
+        //          kind. It appears like the Brackets team were wanting to
+        //          add multiple launchers, which would make sense for multiple
+        //          transports. We might want to consider patching LiveDevMultBrowser
+        //          to allow setting the launcher as well as the transport, and adding
+        //          a launcher of our own rather than hacking on theirs.
+
+        // // send instrumented response or null to fallback to static file
+        // if (liveDocument && liveDocument.getResponseData) {
+        //     response = liveDocument.getResponseData();
+        // } else {
+        //     response = {};  // let server fall back on loading file off disk
+        // }
+        // response.id = request.id;
+
+        // this._send(request.location, response);
+
+
 });
